b12b0b3976eb587c92988d8a4bd3d369ad36842d
Gyllm <guardams@gmail.com>
2020-08-12 21:30:12 -0300

    Atualização 1

--- hello.go
+++ hello.go
@@ -5,13 +5,12 @@ import "fmt"
 func main() {
 	a := 1
 	for a <= 100 {
-		if a%3 == 0 {
-			fmt.Println("fizz")
-			if a%5 == 0 {
-				fmt.Println("fizz buzz")
-			}
+		if a%15 == 0 {
+			fmt.Println("fizz buzz")
 		} else if a%5 == 0 {
 			fmt.Println("buzz")
+		} else if a%3 == 0 {
+			fmt.Println("fizz")
 		} else {
 			fmt.Println(a)
 		}
